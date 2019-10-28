5eefc612a7b717200acb2969f9eae76e3ee29326
Patrick <paddy38@web.de>
2019-10-28 15:46:17 +0100

    Kommentare

--- GccBoardProject1/GccBoardProject1/src/hexapod.h
+++ GccBoardProject1/GccBoardProject1/src/hexapod.h
@@ -2,6 +2,7 @@
 #define HEXAPOD_H
 #include "vector.h"
 #include "quaternion.h"
+#include "hexpod_constants.h"
 
 extern float* calcMotorAngles(struct Vector absTranslation, struct Quaternion absRotation);
 
--- GccBoardProject1/GccBoardProject1/src/hexpod.c
+++ GccBoardProject1/GccBoardProject1/src/hexpod.c
@@ -13,17 +13,22 @@ float gK(float hornLength, struct Vector effectiveRodVector, float rodLength);
 
 float angle(struct Vector effectiveRodVector, float rodLength, float hornLength, float hornRot);
 
+/*
+* Berechnet die Motorwinkel für alle Motoren
+*/
 float* calcMotorAngles(struct Vector absTranslation, struct Quaternion absRotation)
 {
 	float[6] sollWinkel;
 	for (short i = 0; i < 6; i++)
 	{
-		struct Vector effectiveRod = calcEffectiveRodLength(absTranslation, absRotation,
-			motorPositions[i], anchorPositions[i]);
-			sollWinkel[i] = angle(effectiveRod, rodLength, hornLength, hornRotations[i]);
+		struct Vector effectiveRod = calcEffectiveRodLength(absTranslation, absRotation, motorPositions[i], anchorPositions[i]);
+		sollWinkel[i] = angle(effectiveRod, rodLength, hornLength, hornRotations[i]);
 	}
 }
 
+/*
+* Berechnet den Winkel für einen Motor
+*/
 float angle(struct Vector effectiveRodVector, float rodLength, float hornLength, float hornRot)
 {
 	float eK = eK(hornLength, effectiveRodVector);
@@ -32,27 +37,42 @@ float angle(struct Vector effectiveRodVector, float rodLength, float hornLength,
 	return alphaK(eK, fK, gK);
 }
 
+/*
+* Hilfswert eK für Motorwinkel
+*/
 float eK(float hornLength, struct Vector effectiveRodVector)
 {
 	return 2f * hornLength * effectiveRodVector.z;
 }
 
+/*
+* Hilfswert fK für Motorwinkel
+*/
 float fK(float hornLength, struct Vector effectiveRodVector, float hornRotation)
 {
 	return 2f * hornLength * (cosf(hornRotation) * effectiveRodVector.x + sinf(hornRotation) * effectiveRodVector.y);
 }
 
+/*
+* Hilfswert gK für Motorwinkel
+*/
 float gK(float hornLength, struct Vector effectiveRodVector, float rodLength)
 {
 	return powf(magnitude(effectiveRodVector), 2) - rodLength * rodLength - hornLength * hornLength;
 }
 
+/*
+* Berechnet den Motorwinkel für einen Motor
+*/
 float alphaK(float eK, float fK, float gK) 
 {
 	float angle = asinf(gK / sqrtf(eK * eK - fK * fK)) - atan2f(fK, eK);
 	return angle;
 }
 
+/*
+*  Berechnet den Vektor eines imaginären Stabs von der Motorwelle eines Motors bis zum korrespondierenden Ankerpunkt
+*/
 struct Vector calcEffectiveRodLength(
 	struct Vector absTrans, 
 	struct Quaternion absRot, 
